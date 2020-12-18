c3e47cb2015b9399734f463bf9f84d2308c432c3
samanta kouemou <samantakouemou@gmail.com>
2020-12-18 14:25:36 +0100

    first commit

---
+++ arschoolgabon/models.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.db import models
+
+#import uuid
+
+# Create your models here.
+
+class Utilisateur(models.Model):
+    #id_utilisateur=models.UUIDField(primary_key=True, default=uuid.uuid4)
+    login = models.CharField(max_length=100, null=False, blank=False,help_text='Longin...')
+    password = models.CharField(max_length=100, null=False, blank=False,help_text='Mot de passe...')
+    ROLE = (
+    ('e','Enseignant'),
+    ('a','Apprenant'),
+    
+    )
+
+    role = models.CharField(max_length=1,choices = ROLE,blank =True,default ='a',
+    )
+
+    def __str__(self):
+        return f'{self.login}, {self.role}'
